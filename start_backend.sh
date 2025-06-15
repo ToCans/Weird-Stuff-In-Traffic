1fb303d238937335e719eeb74f602b28a832854f
ToCans <thomas.cansfield@gmail.com>
2025-06-15 21:34:58 +0200

    All in one used.

--- start_backend.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Navigate to the backend directory
-cd /home/ai-team2/Weird-Stuff-In-Traffic/App/Backend
-
-# Use the Python binary from your conda environment
-/home/ai-team2/miniconda3/envs/application/bin/python main.py
--- start_frontend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ai-team2/Weird-Stuff-In-Traffic/App/Frontend/weird-traffic-app
-npm run dev -- -H 0.0.0.0
