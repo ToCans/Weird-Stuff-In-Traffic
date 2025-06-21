cd955f7cd5c960c7f31e89735193d4a1126ac3c6
ToCans <thomas.cansfield@gmail.com>
2025-06-21 15:47:18 +0200

    Summary fixes.

--- App/Backend/services/image_summary.py
+++ App/Backend/services/image_summary.py
@@ -22,7 +22,7 @@ def preprocess(instruction: str, image_np: np.ndarray,
         image = image.resize((new_width, new_height))
 
     # System Instructions
-    system_instruction = "You are an assistant that returns a list of objects as strings in the image."
+    system_instruction = "You are an assistant that returns a list of objects as strings in the image. Like so: ['car', 'tree', 'person']"
 
     # Formatting the Chat
     chat = [
